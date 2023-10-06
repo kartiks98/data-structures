9173ed68cdcb9f9d8697f2fde3e55ff344349423
Kartik Saini <kartiksaini135@gmail.com>
2023-10-06 09:07:51 +0530

    sLL pop

--- LinkedList/SinglyLinkedList.js
+++ LinkedList/SinglyLinkedList.js
@@ -33,11 +33,44 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    else {
+      if (this.length === 1) {
+        // if (this.head === this.tail) { //OR
+        this.head = null;
+        this.tail = null;
+        this.length--;
+        return this.head;
+      }
+      let current = this.head;
+      let pre = this.head;
+      // let pre = current; //OR
+      while (!!current.next) {
+        pre = current;
+        current = current.next;
+      }
+      // let current = this.head.next; //OR //NOT RECOMMENDED
+      // let pre = this.head;
+      // while (!!current.next) {
+      //   pre = current;
+      //   current = current.next;
+      // }
+      pre.next = null;
+      this.tail = pre;
+      this.length--;
+      return current;
+    }
+  }
 }
 
 const sLL = new SinglyLinkedList();
 sLL.push("hello");
 sLL.push("World");
 sLL.push("!!!");
+sLL.pop();
+sLL.pop();
+sLL.pop();
+
 sLL.logAsArray();
 console.log(sLL);
