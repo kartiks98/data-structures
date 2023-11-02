e9072baf3e7b85947161c3e4e17df937d189fa0e
Kartik Saini <kartiksaini135@gmail.com>
2023-11-02 09:09:11 +0530

    stack & queue

---
+++ Queue/Queue.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  //Same as Sll push
+  enqueque(val) {
+    if (!this.first) {
+      this.first = new Node(val);
+      this.last = this.first;
+    } else {
+      const newNode = new Node(val);
+      this.last.next = newNode;
+      this.last = newNode;
+      // this.last = this.last.next; //OR
+    }
+    this.size++;
+    return this.size;
+  }
+
+  //Same as Sll shift
+  dequeue() {
+    if (!this.first) return "Invalid Input";
+    if (this.size === 1) {
+      const current = this.first;
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return current.val;
+    }
+    const current = this.first;
+    this.first = this.first.next;
+    // this.first = current.next; //OR
+    this.size--;
+    return current.val;
+  }
+
+  logAsArray() {
+    let current = this.first;
+    const ar = [];
+    while (!!current) {
+      ar.push(current.val);
+      current = current.next;
+    }
+    console.log(ar);
+  }
+}
+
+const queue = new Queue();
+console.log(queue.enqueque("hello"));
+console.log(queue.enqueque("World"));
+console.log(queue.enqueque("!!!"));
+console.log(queue.enqueque("..."));
+
+// console.log(queue.dequeue());
+// console.log(queue.dequeue());
+// console.log(queue.dequeue());
+// console.log(queue.dequeue());
+// console.log(queue.dequeue()); //Invalid Input
+
+queue.logAsArray();
+console.log(queue);
---
+++ Stack/Stack.js
@@ -0,0 +1,70 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  //Same as Sll unshift
+  push(val) {
+    if (!this.first) {
+      this.first = new Node(val);
+      this.last = this.first;
+    } else {
+      const newNode = new Node(val);
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  //Same as Sll shift
+  pop() {
+    if (!this.first) return "Invalid Input";
+    if (this.size === 1) {
+      const current = this.first;
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return current.val;
+    }
+    const current = this.first;
+    this.first = this.first.next;
+    // this.first = current.next; //OR
+    this.size--;
+    return current.val;
+  }
+
+  logAsArray() {
+    let current = this.first;
+    const ar = [];
+    while (!!current) {
+      ar.push(current.val);
+      current = current.next;
+    }
+    console.log(ar);
+  }
+}
+
+const stack = new Stack();
+console.log(stack.push("hello"));
+console.log(stack.push("World"));
+console.log(stack.push("!!!"));
+console.log(stack.push("..."));
+
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log(stack.pop()); //Invalid Input
+
+stack.logAsArray();
+console.log(stack);
