70776110f41e1cecd5081d29d1b55da186a2329e
Kartik Saini <kartiksaini135@gmail.com>
2023-11-15 08:25:23 +0530

    bst insert

--- Tree/BST.js
+++ Tree/BST.js
@@ -10,4 +10,30 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(val, currentRoot) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let current = currentRoot || this.root;
+      if (val >= current.val) {
+        if (!current.right) current.right = newNode;
+        else this.insert(val, current.right);
+      } else {
+        if (!current.left) current.left = newNode;
+        else this.insert(val, current.left);
+      }
+    }
+    return this;
+  }
 }
+
+const bst = new BST();
+console.log(bst.insert(6));
+console.log(bst.insert(2));
+console.log(bst.insert(5));
+console.log(bst.insert(4));
+console.log(bst.insert(3));
+console.log(bst.insert(1));
+console.log(bst.insert(5));
