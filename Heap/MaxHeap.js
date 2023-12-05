127b71627da9dc8e4c2c48f8da7ab9a6355d6de0
Kartik Saini <kartiksaini135@gmail.com>
2023-12-05 08:32:04 +0530

    maxHeap insert

---
+++ Heap/MaxHeap.js
@@ -0,0 +1,30 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(val) {
+    this.heap.push(val);
+    const bubbleUp = (index) => {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[index] > this.heap[parentIndex]) {
+        [this.heap[index], this.heap[parentIndex]] = [
+          this.heap[parentIndex],
+          this.heap[index],
+        ];
+        bubbleUp(parentIndex);
+      }
+    };
+    bubbleUp(this.heap.length - 1);
+    return this.heap;
+  }
+}
+
+const maxHeap = new MaxHeap();
+console.log(maxHeap.insert(41));
+console.log(maxHeap.insert(39));
+console.log(maxHeap.insert(33));
+console.log(maxHeap.insert(18));
+console.log(maxHeap.insert(27));
+console.log(maxHeap.insert(12));
+console.log(maxHeap.insert(55));
