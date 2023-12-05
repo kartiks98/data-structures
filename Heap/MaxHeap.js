252a58ea3c04b8d1d03bf1cbb0561997661e2bd5
Kartik Saini <kartiksaini135@gmail.com>
2023-12-05 08:50:11 +0530

    Update MaxHeap.js

--- Heap/MaxHeap.js
+++ Heap/MaxHeap.js
@@ -21,14 +21,25 @@ class MaxHeap {
 }
 
 const maxHeap = new MaxHeap();
+// console.log(maxHeap.insert(41));
+// console.log(maxHeap.insert(39));
+// console.log(maxHeap.insert(33));
+// console.log(maxHeap.insert(1));
+// console.log(maxHeap.insert(18));
+// console.log(maxHeap.insert(27));
+// console.log(maxHeap.insert(12));
+// console.log(maxHeap.insert(55));
+// console.log(maxHeap.insert(55));
+// console.log(maxHeap.insert(55));
+// console.log(maxHeap.insert(45));
+
 console.log(maxHeap.insert(41));
 console.log(maxHeap.insert(39));
 console.log(maxHeap.insert(33));
-console.log(maxHeap.insert(1));
 console.log(maxHeap.insert(18));
 console.log(maxHeap.insert(27));
 console.log(maxHeap.insert(12));
 console.log(maxHeap.insert(55));
-console.log(maxHeap.insert(55));
-console.log(maxHeap.insert(55));
+console.log(maxHeap.insert(1));
 console.log(maxHeap.insert(45));
+console.log(maxHeap.insert(199));
